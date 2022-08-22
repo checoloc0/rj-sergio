9d0cd83a30047774cc9c779325be480d5cc177d7
checoloco <sergio_orta@hotmail.com>
2022-08-21 22:19:03 -0500

    Desafio 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {Barranav} from './components/NavBar.js'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+    <header>
+        <h1>
+          Tienda de Sneakers by SAOS
+        </h1>
+      
+     
       </header>
-    </div>
+      <Barranav/>
+    </div>   
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+  
+export const Barranav = () =>{
+    return (
+        <section>
+             <nav>            
+              <a>Hombres</a>     
+              <a>Mujeres</a>    
+              <a>Niños</a>                    
+              <a>Hot Fashion</a>  
+              <a>Login</a>      
+              
+             </nav>
+           
+        </section>
+
+    )
+
+
+
+}
+
